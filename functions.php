eee9152719fa1536094e75478d0c6869d9f73e4c
Sittipong Wiboonsirichai <rabbitinlove@gmail.com>
2022-01-31 16:32:52 +0900

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+define( 'RABBITINBLACK_TEMPLATE_DIR', get_template_directory() );
+define( 'RABBITINBLACK_TEMPLATE_DIR_URI', get_template_directory_uri() );
+define( 'RABBITINBLACK_TEXT_DOMAIN', 'rabbitinblack' );
+
+require RABBITINBLACK_TEMPLATE_DIR . '/inc/patterns.php';
